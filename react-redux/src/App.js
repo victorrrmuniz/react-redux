23beb53755cedca324cd91376f3bd3e54a8c9353
Víctor Muniz dos Santos <victor.santos@ituran.com.br>
2021-02-22 13:09:48 -0300

    ajuste nome variável

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
 import './App.css';
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { clickButton } from './actions'
 
 class App extends Component {
 
+  state = {
+    inputValue: ''
+  }
+
+  inputChange = event => {
+    this.setState({
+      inputValue: event.target.value
+    })
+  }
+
+
   render() {
-    const { newValue} = this.props
+    const { newValue, clickButton } = this.props
+
+    const { inputValue } = this.state;
 
     return (
       <div className="App" style={{ paddingTop: '10px' }}>
-        <input type="text" />
-        <button>
+        <input type="text" onChange={this.inputChange} value={inputValue} />
+        <button onClick={() => clickButton(inputValue)}>
           Click Me!
         </button>
         <h1>{ newValue }</h1>
@@ -23,4 +38,7 @@ const mapStateToProps = store => ({
   newValue: store.clickState.newValue
 })
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ clickButton }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
